b39b1bdaa1fc55a9d0ca2f68522fd59e8693114c
Gwen-of-lynn <Gwen.of.lynn@gmail.com>
2021-01-10 17:37:03 +0000

    updated app.py (added Pymongo, and ObjectId, imported: flash, render_template,) , requirements.txt, and created dir templates and essential_oils.html

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World again"
+@app.route("/get_essential_oils")
+def get_essential_oils():
+    essential_oils = mongo.db.essential_oils.find()
+    return render_template(essential_oils.html, essential_oils=essential_oils)
 
 
 if __name__ == "__main__":
