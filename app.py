8898caa683f3177036ab26ea3819419172df451d
Gwen-of-lynn <Gwen.of.lynn@gmail.com>
2021-01-14 15:47:45 +0000

    Allow pageusers to submit essential oils to db

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
-app.config ["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config ["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
@@ -24,13 +24,27 @@ mongo = PyMongo(app)
 def get_essentialoils():
     essentialoils = list(mongo.db.essential_oils.find())
     return render_template(
-        "essentialoils.html", essentialoils = essentialoils)
-#check to make sure variation between essential_oils database and other essentialoils without _ are ok
+        "essentialoils.html", essentialoils=essentialoils)
 
-@app.route("/add_oils")
+
+@app.route("/add_oils", methods=["GET", "POST"])
 def add_oils():
-        categories = mongo.db.categories.find().sort("category_name", 1)
-        return render_template("add_oils.html", categories=categories)
+    if request.method == "POST":
+        source_inc = "on" if request.form.get("source_inc") else "off"
+        oil = {
+            "category_name": request.form.get("category_name"),
+            "oil_name": request.form.get("oil_name"),
+            "oil_description": request.form.get("oil_description"),
+            "source_inc": source_inc,
+        }
+
+        mongo.db.essential_oils.insert_one(oil)
+        flash("Oil Sucessfully Added")
+        return redirect(url_for("get_essentialoils"))
+
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_oils.html", categories=categories)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
