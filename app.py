50c6b4c1bb982b316337917f91fd969055a99d3c
Gwen-of-lynn <Gwen.of.lynn@gmail.com>
2021-01-21 22:16:47 +0000

    added error 404 and 500 html files and in app.py

--- app.py
+++ app.py
@@ -120,6 +120,18 @@ def delete_category(category_id):
     return redirect(url_for("get_categories"))
 
 
+# errorhandler 404 copied and 500 adapted from:
+# https://flask.palletsprojects.com/en/1.1.x/patterns/errorpages/
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
